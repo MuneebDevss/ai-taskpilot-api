f799637e6013b1fc4cc5994c214a51a3cf5d474c
Muneeb Ur Rehman <i222702@nu.edu.pk>
2025-07-12 18:57:01 +0500

    changed the file structure of eslints.config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,45 @@
-module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-    jest: true
-  },
-  extends: [
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-    'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-    'no-console': ['warn', { 'allow': ['warn', 'error'] }],
-    'no-debugger': 'error',
-    'no-duplicate-imports': 'error',
-    'no-var': 'error',
-    'prefer-const': 'error',
-    'prefer-arrow-callback': 'error',
-    'arrow-spacing': 'error',
-    'comma-dangle': ['error', 'never'],
-    'object-curly-spacing': ['error', 'always'],
-    'array-bracket-spacing': ['error', 'never'],
-    'space-before-blocks': 'error',
-    'keyword-spacing': 'error',
-    'space-infix-ops': 'error',
-    'eol-last': 'error',
-    'no-trailing-spaces': 'error',
-    'max-len': ['warn', { 'code': 120 }],
-    'camelcase': ['error', { 'properties': 'always' }]
+// eslint.config.js
+
+import js from '@eslint/js';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+        node: true,
+        jest: true
+      }
+    },
+    plugins: [],
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-duplicate-imports': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'error',
+      'arrow-spacing': 'error',
+      'comma-dangle': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'space-before-blocks': 'error',
+      'keyword-spacing': 'error',
+      'space-infix-ops': 'error',
+      'eol-last': 'error',
+      'no-trailing-spaces': 'error',
+      'max-len': ['warn', { code: 120 }],
+      camelcase: ['error', { properties: 'always' }]
+    }
   }
-};
+];
